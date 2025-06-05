98f0f0fea7e727edfa9b30909c283d614069a00f
Vitnesh Immanuvel <vitne>
2025-06-05 15:12:34 +0530

    8 project dfsdf

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-
+    build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+  base: '/', 
 })
